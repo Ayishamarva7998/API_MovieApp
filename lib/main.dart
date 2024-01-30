80ed7bc01b538057632cd69ccfa62f50c5380c9a
Ayishamarva7998 <ayshaaysha7998@gmail.com>
2024-01-30 14:19:31 +0530

    api successfully completed

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/controller/bottom_provider.dart';
+import 'package:movie_app/controller/home_provider.dart';
+import 'package:movie_app/controller/search_provider.dart';
+import 'package:movie_app/widgets/bottombar.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => BottomBarProvider()),
+          ChangeNotifierProvider(create: (context) => SearchProvider()),
+          ChangeNotifierProvider(create: (context) => HomeProvider()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'MovieFlix',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: BottomNavigation(),
+        ));
+  }
+}
